a9d1aa6c2929c9785bae2796a988ee0ae3842f3e
Miguel Solorio <miguel.solorio@microsoft.com>
2019-10-23 12:36:01 -0700

    Fix #1

--- src/code.ts
+++ src/code.ts
@@ -15,8 +15,8 @@ setTimeout(function(){
       let styleName = figma.getStyleById(String(styleId)).name
       let styleParent = ""
       if (styleName.includes("/")) {
-        styleParent = styleName.substr(0, styleName.indexOf(' /'));
-        styleName = styleName.substring(styleName.indexOf("/") + 2)
+        styleParent = styleName.substr(0, styleName.indexOf('/'))
+        styleName = styleName.substring(styleName.indexOf("/") + 1)
       }
       let colors = children.fills[0].color
       let styleColor = findTheHEX(colors.r, colors.g, colors.b)
